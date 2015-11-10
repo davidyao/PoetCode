dfe66914239cec49f08119593060f2707649c6e1
davidyao <davidjwyao@gmail.com>
2015-11-10 17:01:28 +0800

    Create I.php

---
+++ I.php
@@ -0,0 +1,32 @@
+<?php
+
+final class I 
+extends parents 
+implements human {
+	
+	/**
+	 *@desc If by Life You Were Deceived
+	 *@param 
+	 */
+	protected static attitude function(Life $life){
+		if($life->deceived()){
+		$this->{dismal} = false;
+		$this->{wild} = false;
+
+		foreach($this->days as $day){
+		   $this->{mild} = true;
+		}
+		
+		/**
+If by life you were deceived,
+Don't be dismal,don't be wild!
+In the day of grief,be mild:
+Merry days will come,believe.
+Heart is living in tomorrow;
+Present is dejected here:
+In a moment,passes sorrow;
+That which passes will be dear.
+**/
+	}
+
+}
